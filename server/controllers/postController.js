a88c45a3dc22bd48ee3fe6f06a6019ac5f095165
Riley Bracken <rileybracken@gmail.com>
2017-11-07 14:07:47 -0700

    Finishes contentful integration (#2)

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -4,5 +4,16 @@ exports.getPosts = async (req, res) => {
   const client = initContentful(process.env);
   const data = await client.getEntries({ content_type: 'post' });
 
+  return res.json(data.items);
+};
+
+exports.getPost = async (req, res) => {
+  const client = initContentful(process.env);
+  const data = await client.getEntries({
+    content_type: 'post',
+    include: 2,
+    'fields.slug': req.params.slug,
+  });
+
   return res.json(data.items[0]);
 };
